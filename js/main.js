a4702bc812532d478245a1b978444f44abb04e66
kpaczek <cloudie313@gmail.com>
2021-03-08 18:52:56 +0100

    or

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let a = 11;
+let b= 10;
+
+const numbersCompare = (a,b) => {
+    var x = true;
+    if (a > 20 || b > 20 || (a + b) > 20 ) {
+        return x;
+    }else {
+        return !x;
+    }
+    
+}
+
+console.log(numbersCompare(a,b));
+
