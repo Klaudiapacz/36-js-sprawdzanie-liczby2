fc0f28d0773b2f51bb9efe35392c706c54d88566
kpaczek <cloudie313@gmail.com>
2021-03-15 19:34:00 +0100

    updated

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let b= 10;
 
 const numbersCompare = (a,b) => {
     var x = true;
-    if (a > 20 || b > 20 || (a + b) > 20 ) {
+    if ((a = 20) || (b = 20) || (a + b) <= 20 ) {
         return x;
     }else {
         return !x;
